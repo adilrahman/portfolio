b6c7ca07556ae5d2c0ba34c418dbb409ee7632bb
adilrahman <adilrahman20008@gmail.com>
2022-08-18 04:47:55 +0530

    added home

--- lib/sections/home/home_desktop.dart
+++ lib/sections/home/home_desktop.dart
@@ -1,10 +1,10 @@
 import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
-import 'package:folio/animations/entrance_fader.dart';
-import 'package:folio/configs/configs.dart';
+import 'package:portfolio/animations/entrance_fader.dart';
+import 'package:portfolio/configs/configs.dart';
 
-import 'package:folio/utils/utils.dart';
-import 'package:folio/widget/social_links.dart';
+import 'package:portfolio/utils/utils.dart';
+import 'package:portfolio/widget/social_links.dart';
 
 class HomeDesktop extends StatelessWidget {
   const HomeDesktop({Key? key}) : super(key: key);
