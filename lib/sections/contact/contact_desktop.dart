29d18b0de8f02206572300702094c1b25b1ed8eb
adilrahman <adilrahman20008@gmail.com>
2022-08-18 04:47:05 +0530

    added about and contact

---
+++ lib/sections/contact/contact_desktop.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:portfolio/animations/bottom_animation.dart';
+import 'package:portfolio/configs/configs.dart';
+import 'package:portfolio/utils/contact_utils.dart';
+import 'package:portfolio/widget/custom_text_heading.dart';
+import 'package:portfolio/widget/project_card.dart';
+
+class ContactDesktop extends StatelessWidget {
+  const ContactDesktop({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: Space.all(1, 1),
+      child: Column(
+        children: [
+          const CustomSectionHeading(
+            text: "\nGet in Touch",
+          ),
+          const CustomSectionSubHeading(
+            text: "Let's build something together :)\n\n",
+          ),
+          Space.y!,
+          Wrap(
+              alignment: WrapAlignment.center,
+              runSpacing: AppDimensions.normalize(10),
+              children: ContactUtils.contactIcon
+                  .asMap()
+                  .entries
+                  .map((e) => WidgetAnimator(
+                        child: ProjectCard(
+                          projectIconData: e.value,
+                          projectTitle: ContactUtils.titles[e.key],
+                          projectDescription: ContactUtils.details[e.key],
+                        ),
+                      ))
+                  .toList()),
+        ],
+      ),
+    );
+  }
+}
